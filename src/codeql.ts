1d11050982e42705f943c41267defdbc17bf4cf6
Esben Sparre Andreasen <esbena@github.com>
2021-06-28 13:44:15 +0200

    disable the nice-to-have `print-metrics-summary`

--- src/codeql.ts
+++ src/codeql.ts
@@ -749,7 +749,7 @@ function getCodeQLForCmd(cmd: string): CodeQL {
         "interpret-results",
         threadsFlag,
         "--format=sarif-latest",
-        "--print-metrics-summary",
+        // "--print-metrics-summary",
         "--sarif-group-rules-by-pack",
         "-v",
         `--output=${sarifFile}`,
